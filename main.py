498c325ba5dd0eaeace5c227a53772c0c26b0df5
JeiMZ <J.MEP@outlook.com>
2021-10-04 02:51:44 +0800

    Testing a password length feature

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import random
 
 
-def GeneratePassword():
+def GeneratePassword(passwordLength):
     password = ""
-    for n in range(10):
-        password += chr(random.randint(48, 126))
-    return password
+
+    if 0 < passwordLength < 50:
+        for n in range(passwordLength):
+            password += chr(random.randint(48, 126))
+        return password
+    else:
+        print("> Invalid password length, Please enter a number between 1-50")
+        return
+
+
 
 
 def main():
-   Password = GeneratePassword()
+    print("> Hello, Welcome to: Password Generator ")
 
-   print(Password)
+    passwordLength = int(input("> Enter a Password Length [1-50]: "))
+    Password = GeneratePassword(passwordLength)
+
+    if Password  != None:
+        print(Password)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
